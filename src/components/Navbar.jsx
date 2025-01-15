4cfc545345af243d0a8ce690328207929f004dd7
Gilberto Rodriguez <gilrod1017@gmail.com>
2025-01-14 23:19:19 -0600

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
-import '../styles/Navbar.css'
-import logo from '../assets/tacoymas.png'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navbar.css";
 
-function Navbar() {
+function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-    <div className='nav-container'>
-      <div className='nav-content-container'>
-        <img src={logo} className='company-logo' alt='Company Logo'/>
-
-        <div>
-          Menu
-        </div> 
-
-        <div>
-          About Us
-        </div>  
-
-        <div>
-          FAQ
-        </div>
+    <nav className="navbar">
+      <div className="logo">Company Logo</div>
+      <div
+        className={`menu-icon ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
-    </div>
-    </>
-  )
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li>
+          <NavLink to="/" onClick={toggleMenu}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" onClick={toggleMenu}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/portfolio" onClick={toggleMenu}>
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/blog" onClick={toggleMenu}>
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" onClick={toggleMenu}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default HamburgerMenu;
