423649f681cccd5403926d37b34b79bab9938f11
Gilberto Rodriguez <gilrod1017@gmail.com>
2025-01-21 20:54:48 -0600

    navbar hamburger toggle fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
 import logo from '../assets/tacoymas.png'
 
 function HamburgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1100);
+  
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1100);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
+    if (!isLargeScreen) {
+      setIsOpen((prev) => !prev);
+    }
   };
 
   return (
